0ddbbb3bc97bf932e48fa2daccac2a9d703c94e1
Konstantin Ogarkov <waw909waw@mail.ru>
2026-09-06 21:33:55 +0700

    fix: Test Connection now shows result text with correct visibility and color

---
+++ Client/Converters/ConnectionStatusToColorConverter.cs
@@ -0,0 +1,27 @@
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AiDesktopClient.Converters;
+
+public sealed class ConnectionStatusToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string status)
+        {
+            return status.ToLowerInvariant() switch
+            {
+                "success" => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5CFCB0")),
+                "error" or "failed" => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FC5C7C")),
+                _ => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8B8D9E")),
+            };
+        }
+        return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8B8D9E"));
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
